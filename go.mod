9776dbcc5f098f9e1519a53f7e17283f24a84c91
teguh-satriya <teguh.satriya.w@gmail.com>
2021-09-26 01:06:13 +0700

    add mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/teguh-satriya/ocr-test
+
+go 1.17
+
+require github.com/otiai10/gosseract/v2 v2.3.1
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/otiai10/gosseract/v2"
 )
 
 func main() {
 	client := gosseract.NewClient()
 	defer client.Close()
-	client.SetImage("path/to/image.png")
+	client.SetImage("Untitled.png")
 	text, _ := client.Text()
 	fmt.Println(text)
 	// Hello, World!
